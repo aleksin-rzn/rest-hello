dd58c5c003cfac19d16c545cff32120977850c2f
aleksin-rzn <aleksin.rzn@gmail.com>
2020-05-02 17:05:47 +0300

    Update HelloResource.kt
    Test merge into master

--- src/main/kotlin/ru/docker/rest/dockerrest/HelloResource.kt
+++ src/main/kotlin/ru/docker/rest/dockerrest/HelloResource.kt
@@ -16,6 +16,6 @@ class HelloResource {
     @GetMapping(value = "/hello/{message}")
     fun hello(@PathVariable message: String): String{
         logger.info("GET api/hello/$message")
-        return "Hi1, $message! Your guid: ${UUID.randomUUID().toString()}"
+        return "Test merge into master, $message! Your guid: ${UUID.randomUUID().toString()}"
     }
 }
