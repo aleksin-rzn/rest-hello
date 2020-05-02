e5a62bcdf5d59f657f1af8006eeb2e77c6d44e09
aleksin-rzn <aleksin.rzn@gmail.com>
2020-05-03 00:20:57 +0300

    Update HelloResource.kt
    Fake Merge

--- src/main/kotlin/ru/docker/rest/dockerrest/HelloResource.kt
+++ src/main/kotlin/ru/docker/rest/dockerrest/HelloResource.kt
@@ -16,6 +16,6 @@ class HelloResource {
     @GetMapping(value = "/hello/{message}")
     fun hello(@PathVariable message: String): String{
         logger.info("GET api/hello/$message")
-        return "Test merge into,  $message! Your guid: ${UUID.randomUUID().toString()}"
+        return "Fake merge,  $message! Your guid: ${UUID.randomUUID().toString()}"
     }
 }
